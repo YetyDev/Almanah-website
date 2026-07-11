77cd97275eb47b193f00319a432db6ac3490c51f
Almanah Foundation <info@almanahcarefoundation.com.ng>
2026-07-11 02:29:31 -0700

    Update terms page and site views

---
+++ app.js
@@ -0,0 +1,5 @@
+// This file serves as an entry point redirecting to server.js.
+// It is useful for hosting environments (like cPanel, Passenger, or Vercel)
+// that expect 'app.js' as the default startup/entry file.
+
+require('./server.js');
